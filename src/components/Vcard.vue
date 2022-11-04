c36d796c9b7ec3ea542c9387674c29c96506b447
xlanex6 <alexduval71@gmail.com>
2022-11-04 13:44:25 +0100

    Cahnge file

--- src/components/Vcard.vue
+++ src/components/Vcard.vue
@@ -6,7 +6,7 @@ const fileRender = isAppleMobile ? '/alx.ics' : '/alx.vcard'
 <template>
 	
 	<li class="bg-neutral-500  flex justify-center w-full rounded-lg py-2 hover:cursor-pointer">
-		<a :href="fileRender" class="hover:cursor-pointer h-16 grid content-center text-white text-3xl">
+		<a href="/vcard" class="hover:cursor-pointer h-16 grid content-center text-white text-3xl">
 			VCARD
 			<!-- <Icon class="h-16 w-16 text-white" name="mdi:abacus"/> -->
 			</a>
