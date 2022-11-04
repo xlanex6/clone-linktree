05257e9d14e035dee323d5a5584619a122b27292
xlanex6 <alexduval71@gmail.com>
2022-11-04 12:10:36 +0100

    Manage VCARD as dedicate comp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import vue from "@astrojs/vue";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), vue()]
 });
---
+++ src/components/Vcard.vue
@@ -0,0 +1,15 @@
+<script setup>
+const isAppleMobile = ['iPad', 'iPhone', 'iPod'].includes(navigator.userAgentData.platform)
+const fileRender = isAppleMobile ? '/alx.ics' : '/alx.vcard'
+</script>
+
+<template>
+	
+	<li class="bg-neutral-500  flex justify-center w-full rounded-lg py-2 hover:cursor-pointer">
+		<a :href="fileRender" class="hover:cursor-pointer h-16 grid content-center text-white text-3xl">
+			VCARD
+			<!-- <Icon class="h-16 w-16 text-white" name="mdi:abacus"/> -->
+			</a>
+		</li>
+
+</template>
