b112e1316c58f242d83686802172e87eb7ac1ae9
xlanex6 <alexduval71@gmail.com>
2022-11-04 12:15:44 +0100

    Add netlify for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from "@astrojs/image";
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), vue()]
+  integrations: [tailwind(), image(), vue()],
+  output: "server",
+  adapter: netlify()
 });
