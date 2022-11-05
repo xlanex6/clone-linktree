569af7d4f560a7b8ef4617434c0cd2cf22f81f8a
xlanex6 <alexduval71@gmail.com>
2022-11-05 08:57:19 +0100

    No serverless function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from "@astrojs/vue";
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
-  output: 'server',
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url)
-  })
+  integrations: [tailwind(), vue()]
 });
--- src/components/Vcard.vue
+++ src/components/Vcard.vue
@@ -1,15 +1,40 @@
 <script setup>
 const isAppleMobile = ['iPad', 'iPhone', 'iPod'].includes(navigator.userAgentData.platform)
 const fileRender = isAppleMobile ? '/alx.ics' : '/alx.vcard'
+
+import VCard from 'vcard-creator'
+import { saveAs } from 'file-saver';
+
+  const myVard = new VCard()
+  myVard
+    .addEmail('alexduval71@gmail.com')
+    .addName("DUVAL", 'Alex')
+    .addPhoneNumber('+33620512354', 'WORK')
+    .addJobtitle('Web Developer')
+    .addRole('CTO as Service')
+    .addURL('http://www.alexduval.fr')
+  
+const vcard = myVard.toString()
+
+function pdfFinish() {
+      const url = window.URL.createObjectURL(new Blob([vcard]))
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', 'Alex.vcard') //or any other extension
+      document.body.appendChild(link)
+      link.click()
+}
+
+
 </script>
 
 <template>
 	
 	<li class="bg-neutral-500  flex justify-center w-full rounded-lg py-2 hover:cursor-pointer">
-		<a href="/vcard" class="hover:cursor-pointer h-16 grid content-center text-white text-3xl">
+		<button @click="pdfFinish" class="hover:cursor-pointer h-16 grid content-center text-white text-3xl">
 			VCARD
 			<!-- <Icon class="h-16 w-16 text-white" name="mdi:abacus"/> -->
-			</a>
+		</button>
 		</li>
 
 </template>
